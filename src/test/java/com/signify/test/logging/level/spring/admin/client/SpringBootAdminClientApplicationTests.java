e707bc310bc24135357bb8318a98dd21aa6c51aa
saumen das <63276757+saumen-das1984@users.noreply.github.com>
2020-10-29 11:25:45 +0530

    Initial Commit
    Initial Commit

---
+++ src/test/java/com/signify/test/logging/level/spring/admin/client/SpringBootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signify.test.logging.level.spring.admin.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
